1253e7c6692109d645c4ed461ff19d21ea87161b
dastasoft <dastasoft@gmail.com>
2020-08-19 12:10:11 +0200

    added svg integration

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import NextLogo from '../public/nextjs.svg';
+
 export default function Home() {
   return (
-    <div className="p-4 mx-auto">
-      <h1>Next.js Boilerplate</h1>
+    <div className="p-4 mx-auto flex items-center">
+      <NextLogo className="mr-4" width="100px" height="100px" />
+      <span>Boilerplate</span>
     </div>
   );
 }
