e97bc29124cb353b2383b804953822e25ea3b0b1
dastasoft <dastasoft@gmail.com>
2020-10-22 13:11:01 +0200

    react 17 eslint standard

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,20 @@
 module.exports = {
   env: {
     browser: true,
-    es2020: true,
-    node: true
+    es2021: true,
+    node: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
+  extends: ["plugin:react/recommended", "standard", "prettier"],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
-    ecmaVersion: 11,
-    sourceType: 'module'
+    ecmaVersion: 12,
+    sourceType: "module",
   },
-  plugins: ['react'],
+  plugins: ["react"],
   rules: {
-    'react/react-in-jsx-scope': 'off',
-    'react/display-name': 'off',
-    'react/prop-types': 'off',
-    'react/jsx-props-no-spreading': 'off',
-    'jsx-a11y/anchor-is-valid': [
-      'error',
-      {
-        components: ['Link'],
-        specialLink: ['hrefLeft', 'hrefRight'],
-        aspects: ['invalidHref', 'preferButton']
-      }
-    ],
-    'jsx-a11y/href-no-hash': ['off'],
-    'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }]
-  }
-};
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+  },
+}
