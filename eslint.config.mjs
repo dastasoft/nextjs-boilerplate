c47ab9b58eac0560d7dc0b23a87f00f9299a5709
dastasoft <45243062+dastasoft@users.noreply.github.com>
2025-09-16 08:34:13 +0000

    fix: eslint tailwind 4 support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
-import { dirname } from "node:path";
-import { fileURLToPath } from "node:url";
-import antfu from "@antfu/eslint-config";
-import jsxA11y from "eslint-plugin-jsx-a11y";
-import tailwind from "eslint-plugin-tailwindcss";
+import antfu from '@antfu/eslint-config'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import tailwind from 'eslint-plugin-tailwindcss'
+import { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
 export default antfu(
   {
@@ -14,7 +14,7 @@ export default antfu(
     stylistic: {
       indent: 2,
       semi: false,
-      quotes: "single",
+      quotes: 'single',
     },
 
     formatters: {
@@ -23,7 +23,7 @@ export default antfu(
   },
 
   jsxA11y.flatConfigs.recommended,
-  ...tailwind.configs["flat/recommended"],
+  ...tailwind.configs['flat/recommended'],
   {
     settings: {
       tailwindcss: {
@@ -33,18 +33,18 @@ export default antfu(
   },
   {
     rules: {
-      "antfu/no-top-level-await": "off",
-      "no-console": ["warn"],
-      "style/brace-style": ["error", "1tbs"],
-      "ts/consistent-type-definitions": ["error", "type"],
-      "react/prefer-destructuring-assignment": "off",
-      "node/prefer-global/process": "off",
-      "perfectionist/sort-imports": [
-        "error",
+      'antfu/no-top-level-await': 'off',
+      'no-console': ['warn'],
+      'style/brace-style': ['error', '1tbs'],
+      'ts/consistent-type-definitions': ['error', 'type'],
+      'react/prefer-destructuring-assignment': 'off',
+      'node/prefer-global/process': 'off',
+      'perfectionist/sort-imports': [
+        'error',
         {
-          tsconfigRootDir: ".",
+          tsconfigRootDir: '.',
         },
       ],
     },
-  }
-);
+  },
+)
