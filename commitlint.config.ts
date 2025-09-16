3c8c301fda57e6eda1a24ee3c3b9672ee4cd220f
dastasoft <dastasoft@protonmail.com>
2025-09-16 17:52:00 +0900

    fix: lint issues

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,8 +1,8 @@
-import type { UserConfig } from '@commitlint/types';
+import type { UserConfig } from '@commitlint/types'
 
 const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
   ignores: [message => message.startsWith('chore: bump')], // Ignore dependabot commits
-};
+}
 
-export default Configuration;
+export default Configuration
